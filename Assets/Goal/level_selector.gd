214544c0f3734e462beaa8e409f2110963850398
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-11-06 23:16:48 -0800

    started goal object

---
+++ Assets/Goal/level_selector.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var levels_dir = DirAccess.open("res://Assets/Levels/")
+
+const file_extension : String = "res://Assets/Levels/"
+
+var files_array : Array = []
+
+func _ready() -> void:
+	files_array = levels_dir.get_files()
+
+func _on_body_entered(body: CharacterBody2D) -> void:
+	if (body.name == "Player"):
+		get_tree().change_scene_to_file(file_extension + files_array.pick_random())
+		
