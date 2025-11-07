78bd4f4a9dc53263392f3fc4c999caa6c6283ad5
jaredNussbaum <jared.nussbaum.04@gmail.com>
2025-11-07 00:34:53 -0800

    added random sfx for goal completion, changed logic for transitions on level selection

--- Assets/Goal/level_selector.gd
+++ Assets/Goal/level_selector.gd
@@ -6,10 +6,32 @@ const file_extension : String = "res://Assets/Levels/"
 
 var files_array : Array = []
 
+@onready var AUDIO : AudioStreamPlayer2D = $"Win SFX"
+var dir := DirAccess.open("")
+
+
 func _ready() -> void:
 	files_array = levels_dir.get_files()
+	choose_jump_sound()
 
 func _on_body_entered(body: CharacterBody2D) -> void:
 	if (body.name == "Player"):
-		get_tree().change_scene_to_file.call_deferred(file_extension + files_array.pick_random())
+		AUDIO.play()
 		
+func _on_win_sfx_finished() -> void:
+	get_tree().change_scene_to_file.call_deferred(file_extension + files_array.pick_random())
+
+
+
+func choose_jump_sound():
+	var win_sfx : Array = []
+	
+	dir.list_dir_begin()
+	for file: String in dir.get_files():
+		if ".import" not in file:
+			var resource := dir.get_current_dir() + "/" + file
+			win_sfx.append(resource)
+			
+	randomize()
+	win_sfx.shuffle()
+	AUDIO.stream = load(win_sfx[0])
