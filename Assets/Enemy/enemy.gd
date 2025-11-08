0e90914146cab0c409d071605b1fd058cc3f9b12
Ivy Dudzik <ivydudzik@gmail.com>
2025-11-07 16:17:11 -0800

    Update Naming Conventions for Readability

--- Assets/Enemy/enemy.gd
+++ Assets/Enemy/enemy.gd
@@ -5,7 +5,7 @@ var enemy_scripts_dir = DirAccess.open("res://Assets/Enemy/Scripts/")
 const file_extension: String = "res://Assets/Enemy/Scripts/"
 var files_array: PackedStringArray
 var scripts_array: Array[String] = []
-var custom_script: EnemyScript
+var custom_script: EnemyBehaviorScript
 
 ## WARNING: DO NOT MODIFY THIS FILE
 ## WARNING: DO NOT MODIFY THIS FILE
@@ -22,10 +22,11 @@ func _ready() -> void:
 	# print(scripts_array)
 	custom_script = load(file_extension + scripts_array.pick_random()).new()
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if !%Player: 
 		printerr("[WARNING] Enemy requires that a node 'Player' exists within the same scene that has 'Access as Unique Name' enabled")
 		get_tree().quit()
 	else:
+		# Call the custom move function from the selected EnemyScript
 		linear_velocity = custom_script.move(global_position, %Player.global_position, delta)
 	
